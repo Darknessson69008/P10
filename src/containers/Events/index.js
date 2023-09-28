5ffc049ad4863d611e0bb3100591ec3c85f25a3e
Darknessson69008 <131368078+Darknessson69008@users.noreply.github.com>
2023-09-28 15:52:23 +0200

    modified:   src/components/Select/index.js 	modified:   src/containers/Events/index.js 	modified:   src/containers/Form/index.js 	modified:   src/containers/Slider/index.js 	modified:   src/pages/Home/index.js

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,7 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter((event) => event.type === type)) || [] // ajout selection de newvalue via Select
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
